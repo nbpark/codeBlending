2d296ed044abebce8d8608fa6ad5e9d23119a4d1
주보화 <rumi@bliex.com>
2019-12-07 18:26:17 +0900

    router

---
+++ src/components/GlobalNav.vue
@@ -0,0 +1,25 @@
+<template>
+   <v-app-bar
+      app
+      dark
+    >
+      <div>code Blending</div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        target="_blank"
+        text
+      >
+        <span class="mr-2">btn area</span>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    
+  }),
+};
+</script>
